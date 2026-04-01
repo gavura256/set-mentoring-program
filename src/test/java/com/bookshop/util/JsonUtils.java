8a9f0edaad327a460182013bd6c2ba99f9cf9475
Kostiantyn Gavura <gavura@duck.com>
2026-04-01 19:30:56 +0200

    add JsonUtils for JSON serialization and deserialization

---
+++ src/test/java/com/bookshop/util/JsonUtils.java
@@ -0,0 +1,31 @@
+package com.bookshop.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JsonUtils {
+
+    private final ObjectMapper objectMapper;
+
+    public JsonUtils(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    public String toJson(Object obj) {
+        try {
+            return objectMapper.writeValueAsString(obj);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> T fromJson(String json, Class<T> clazz) {
+        try {
+            return objectMapper.readValue(json, clazz);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
