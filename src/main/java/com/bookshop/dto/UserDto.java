3c16bf92e18c9d6045069d5c0a270cbf363cb40e
Kostiantyn Gavura <gavura@duck.com>
2026-04-16 17:17:31 +0200

    refactor UserDto: remove unused imports and clean up validation annotations

--- src/main/java/com/bookshop/dto/UserDto.java
+++ src/main/java/com/bookshop/dto/UserDto.java
@@ -3,15 +3,14 @@ package com.bookshop.dto;
 import com.bookshop.model.enums.Role;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import java.io.Serializable;
 
 @Data
