d086c088a1bc3876d9e6ef68e28d2c5a9b6d8167
Kostiantyn Gavura <gavura@duck.com>
2026-05-03 13:19:34 +0200

    refactor: reorder imports and fix formatting in BookingController and UserController

--- src/main/java/com/bookshop/controller/UserController.java
+++ src/main/java/com/bookshop/controller/UserController.java
@@ -5,14 +5,14 @@ import com.bookshop.dto.UserResponse;
 import com.bookshop.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,9 +21,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
-import jakarta.validation.Valid;
-import org.springframework.security.access.prepost.PreAuthorize;
 
 @RestController
 @RequestMapping(ApiRoutes.USERS)
